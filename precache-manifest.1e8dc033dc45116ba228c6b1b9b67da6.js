322076410d78f7b3c736e59adb37eb29422c0162
Lucas <lucas@osetfoundation.org>
2019-11-12 12:56:57 -0500

    Updates

---
+++ precache-manifest.1e8dc033dc45116ba228c6b1b9b67da6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a811200f61278e4f3b4cd64f7c225f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2828bf784a27e3f5fc5e",
+    "url": "/static/css/main.88ca6766.chunk.css"
+  },
+  {
+    "revision": "e956336d709bb1f5567e",
+    "url": "/static/js/2.038597b5.chunk.js"
+  },
+  {
+    "revision": "2828bf784a27e3f5fc5e",
+    "url": "/static/js/main.11bc5d35.chunk.js"
+  },
+  {
+    "revision": "dbe9a84a4679dec3fd97",
+    "url": "/static/js/runtime-main.524f2c9c.js"
+  }
+]);
